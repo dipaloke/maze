5f67ddeb26a2861e3c3012fcefa6558310886d8d
Dipaloke <dipalokebiswas96@gmail.com>
2025-11-02 15:33:59 +0600

    Created the bare bone structure of the app.

--- src/main/java/com/example/maze/Launcher.java
+++ src/main/java/com/example/maze/Launcher.java
@@ -1,7 +1,7 @@
 package com.example.maze;
 
 import javafx.application.Application;
-
+// This is the main entry point
 public class Launcher {
     public static void main(String[] args) {
         Application.launch(App.class, args);
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,17 @@
 module com.example.maze {
     requires javafx.controls;
     requires javafx.fxml;
+    requires de.jensd.fx.glyphs.fontawesome;
+    requires java.sql;
+    requires  org.xerial.sqlitejdbc;
 
 
     opens com.example.maze to javafx.fxml;
     exports com.example.maze;
+    exports com.example.maze.Controllers;
+    exports com.example.maze.Controllers.Admin;
+    exports com.example.maze.Controllers.Client;
+    exports com.example.maze.Models;
+    exports com.example.maze.Views;
+
 }
