d1a3cae36ba2b1645e1ce6ecdfb1f60de3d98ba6
Dipaloke <dipalokebiswas96@gmail.com>
2025-11-03 15:06:31 +0600

    Client menu: sidebar : Creaeted the frontned in scene builder, then implemented custom css lastly integrated with ClientMenuController.

--- src/main/java/com/example/maze/Controllers/Client/ClientMenuController.java
+++ src/main/java/com/example/maze/Controllers/Client/ClientMenuController.java
@@ -1,4 +1,21 @@
 package com.example.maze.Controllers.Client;
 
-public class ClientMenuController {
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClientMenuController implements Initializable {
+    public Button dashboard_btn;
+    public Button transection_btn;
+    public Button accounts_btn;
+    public Button profile_btn;
+    public Button logout_btn;
+    public Button report_btn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
